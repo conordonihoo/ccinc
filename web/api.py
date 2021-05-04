c48eeb0dd0b43c55a11a6b7c8a8c8cf4c1f2a026
Conor Donihoo <conordonihoo@hotmail.com>
2021-05-03 23:14:27 -0500

    added routes - UNTESTED

--- web/api.py
+++ web/api.py
@@ -5,13 +5,62 @@ import jobs
 import redis
 
 app = Flask(__name__)
-rd = redis.StrictRedis(host="10.102.92.99", port=6379, db=1)
-
 
 @app.route('/')
 def main():
-    # We use the HTML file as the template
-    return render_template("page.html")
+  # We use the HTML file as the template
+  return render_template("page.html")
+
+@app.route('/login?id=ID_STRING_HERE', methods=['GET'])
+def login():
+  bid = request.args.get('id', default='', type=str)
+  if jobs.bid_exists(bid): # make bid_exists() method in jobs.py
+    return json.dumps(jobs.rd.hgetall(bid))
+  else:
+    return 'ACCOUNT NUMBER NOT FOUND'
+
+@app.route('/create', methods=['GET'])
+def create():
+  bid = request.args.get('id', default='', type=str)
+  jobs.create() # make create() method in jobs.py
+  return json.dumps(jobs.rd.hget('bid'))
+
+@app.route('/ids', methods=['GET'])
+def ids():
+  return json.dumps(jobs.rd.keys())
+
+@app.route('/delete?id=ID_STRING_HERE', methods=['GET'])
+def delete():
+  bid = request.args.get('id', default='', type=str)
+  if jobs.bid_exists(bid): # make bid_exists() method in jobs.py
+    jobs.rd.delete(bid)
+    return 'completed'
+  else:
+    return 'ACCOUNT NUMBER NOT FOUND'
+
+@app.route('/transaction/deposit?id=ID_STRING_HERE&amount=AMOUNT_INT_HERE', methods=['GET'])
+def deposit():
+  bid = request.args.get('id', default='', type=str)
+  amount = request.args.get('amount', default=0, type=float)
+  if jobs.bid_exists(bid): # make bid_exists() method in jobs.py
+    jobs.deposit(bid,amount) # make deposit() method in jobs.py
+    return ('Balance: ' + jobs.rd.hget(bid,'balance'))
+  else:
+    return 'ACCOUNT NUMBER NOT FOUND'
+
+@app.route('/transaction/withdraw?id=ID_STRING_HERE&amount=AMOUNT_INT_HERE', methods=['GET'])
+def withdraw():
+  bid = request.args.get('id', default='', type=str)
+  amount = request.args.get('amount', default=0, type=float)
+  if jobs.bid_exists(bid): # make bid_exists() method in jobs.py
+    if jobs.can_withdraw(bid,amount): # make can_withdraw() method in jobs.py
+      jobs.withdraw(bid,amount) # make withdraw() method in jobs.py
+      return ('Balance: ' + jobs.rd.hget(bid,'balance'))
+    else:
+      return 'NOT ENOUGH BALANCE'
+  else:
+    return 'ACCOUNT NUMBER NOT FOUND'
+
 
 
 if __name__ == '__main__':
